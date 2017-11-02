db257fbe36d8243cd3dce69bb586f87b2c71b05d
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-02 09:48:27 +0200

    ty

---
+++ Mamma.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat
+{
+    class Mammal
+    {
+        public Mammal
+    }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int RealYears { get; set; }
+    public Dog()
+    {
+        Name = "Rex";
+        Age = 2;
+        Color = "Brown";
+        CalculateDogYears();
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Auh auh!!!");
+    }
+
+    public int CalculateDogYears()
+    {
+        RealYears = Age * 7;
+        return RealYears;
+    }
